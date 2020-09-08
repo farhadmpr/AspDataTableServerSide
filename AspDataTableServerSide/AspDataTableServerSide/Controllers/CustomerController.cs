fcb8bea7efcad8707644367e7fa87dba365cb0ee
farhadmpr <farhadmp@gmail.com>
2020-09-08 11:45:17 +0430

    Add CustomerController code

--- AspDataTableServerSide/AspDataTableServerSide/Controllers/CustomerController.cs
+++ AspDataTableServerSide/AspDataTableServerSide/Controllers/CustomerController.cs
@@ -1,16 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using AspDataTableServerSide.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspDataTableServerSide.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext context;
+        public CustomerController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
         public IActionResult Index()
         {
-            return View();
+            try
+            {
+                var draw = Request.Form["draw"].FirstOrDefault();
+                var start = Request.Form["start"].FirstOrDefault();
+                var length = Request.Form["length"].FirstOrDefault();
+                var sortColumn = Request.Form["columns[" + Request.Form["order[0][column]"].FirstOrDefault() + "][name]"].FirstOrDefault();
+                var sortColumnDirection = Request.Form["order[0][dir]"].FirstOrDefault();
+                var searchValue = Request.Form["search[value]"].FirstOrDefault();
+
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+
+                var customerData = (from tempcustomer in context.Customers select tempcustomer);
+
+                if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
+                {
+                    customerData = customerData.OrderBy(sortColumn + " " + sortColumnDirection);
+                }
+
+                if (!string.IsNullOrEmpty(searchValue))
+                {
+                    customerData = customerData.Where(m => m.FirstName.Contains(searchValue)
+                                                || m.LastName.Contains(searchValue)
+                                                || m.Contact.Contains(searchValue)
+                                                || m.Email.Contains(searchValue));
+                }
+
+                recordsTotal = customerData.Count();
+                var data = customerData.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+
+                return Ok(jsonData);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
